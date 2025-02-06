c73e46448f3b0de31b5aa33ea0d6423c4ebe3851
Jonamehmeti1 <jona.mehmeti@student.uni-pr.edu>
2025-02-06 20:14:16 +0100

    Krijimi i Account

--- projektivegla.cpp
+++ projektivegla.cpp
@@ -1,54 +1,16 @@
 #include <iostream>
-#include <iomanip>  
-
+#include <string>
 using namespace std;
 
-class BankAccount {
-private:
+struct BankAccount {
+    string accountNumber;
+    string accountHolderName;
     double balance;
-
-public:
-    // Per inicializim te bilancit
-    BankAccount(double initialBalance) {
-        if (initialBalance >= 0) {
-            balance = initialBalance;
-        } else {
-            cout << "Bilanci inicial nuk mund të jetë negativ." << endl;
-            balance = 0;
-        }
-    }
-
-    // Per depozitim
-    void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            cout << "Keni depozituar me sukses: $" << fixed << setprecision(2) << amount << endl;
-            cout << "Bilanci aktual: $" << fixed << setprecision(2) << balance << endl;
-        } else {
-            cout << "Shuma duhet të jetë më e madhe se zero." << endl;
-        }
-    }
-
-    // Metoda qe e kthen bilancin
-    double getBalance() {
-        return balance;
-    }
 };
 
-int main() {
-    double initialBalance = 1000.00;  // Bilanci fillestar ne llogari
-    BankAccount account(initialBalance);
-
-    cout << "Mirësevini në Bankë!" << endl;
-    cout << "Bilanci juaj aktual: $" << fixed << setprecision(2) << account.getBalance() << endl;
-
-    
-    double depositAmount;
-    cout << "Shkruani shumën që dëshironi të depozitoni: $";
-    cin >> depositAmount;
-
-    //Thirrja e metodes deposit
-    account.deposit(depositAmount);
-
-    return 0;
+void createAccount(BankAccount &account, const string &accNum, const string &accHolder, double initBalance) {
+    account.accountNumber = accNum;
+    account.accountHolderName = accHolder;
+    account.balance = initBalance;
+    std::cout << "Account created successfully!\n";
 }
